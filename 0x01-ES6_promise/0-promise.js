e258aa7ddd43791291c725cc43a3bf16fc02829d
Daniel Williams <williamsoligodaniel@gmail.com>
2023-01-26 04:48:20 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,1 +1,11 @@
 //only make promise
+const getResponseFromAPI = new Promise((resolve, reject) => {
+  let responseFromAPI = true;
+  if (responseFromAPI) {
+    resolve();
+  } else {
+    reject();
+  }
+  /* eslint-enable */
+})
+export default getResponseFromAPI;
