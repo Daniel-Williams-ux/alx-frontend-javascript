0e55a145aed279b5cfdbfa2117e3496371e05e5a
Daniel Williams <williamsoligodaniel@gmail.com>
2023-01-27 04:50:47 +0100

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,19 +1,16 @@
 export default class Car {
-  constructor(brand, motor, color, range) {
+  constructor(brand, motor, color) {
     if (typeof brand !== 'string') {
       throw new TypeError('Brand must be a string');
     } else if (typeof motor !== 'string') {
       throw new TypeError('Motor must be a string');
     } else if (typeof color !== 'string') {
       throw new TypeError('Color must be a string');
-    } else if (typeof range !== 'string) {
-      throw new TypeError('Range must be a string');
     }
 
     this._brand = brand;
     this._motor = motor;
     this._color = color;
-    this._range = range;
   }
 
   cloneCar() {
