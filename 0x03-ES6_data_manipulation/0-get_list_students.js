195bf61651087fe4f9662e0c9c0b785ce4d704de
Daniel Williams <williamsoligodaniel@gmail.com>
2023-01-30 14:39:28 +0100

    Create 0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+const getListStudents = () => {
+  let newArr = [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco'
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 3,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    }
+  ]
+    return newArr;
+}
+export default getListStudents();
