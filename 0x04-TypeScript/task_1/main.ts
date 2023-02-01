2f2f20fbca99b0dc08b0eb89398cef17d919600f
Daniel Williams <williamsoligodaniel@gmail.com>
2023-02-01 19:20:38 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/main.ts
+++ 0x04-TypeScript/task_1/main.ts
@@ -14,3 +14,25 @@ const teacher: Teacher = {
   location: "London",
   contract: true,
 };
+
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Director extends Teacher {
+  numberOfReports: number;
+}
+
+const director: Director = {
+  firstName: 'John',
+  lastName: 'Smith',
+  fullTimeEmployee: true,
+  location: 'New York',
+  numberOfReports: 5,
+  contract: false
+};
