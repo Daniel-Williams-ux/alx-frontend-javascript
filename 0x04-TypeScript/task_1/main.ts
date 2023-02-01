32a69583558095364a1ac6f8e5bce87744aff61b
Daniel Williams <williamsoligodaniel@gmail.com>
2023-02-01 19:33:25 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/main.ts
+++ 0x04-TypeScript/task_1/main.ts
@@ -36,3 +36,45 @@ const director: Director = {
   numberOfReports: 5,
   contract: false
 };
+
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Director extends Teacher {
+  numberOfReports: number;
+}
+
+const director: Director = {
+  firstName: 'John',
+  lastName: 'Smith',
+  fullTimeEmployee: true,
+  location: 'New York',
+  numberOfReports: 5,
+  contract: false
+};
+
+const teacher: Teacher = {
+  firstName: 'Jane',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  location: 'San Francisco',
+  contract: true
+};
+
+const printTeacher = (firstName: string, lastName: string): string => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printDirector: printTeacherFunction = printTeacher;
+console.log(printDirector(director.firstName, director.lastName));
+console.log(printTeacher(teacher.firstName, teacher.lastName));
