b224bbc3756f39b2773689baf8d902746e45faed
Daniel <williamsoligodaniel@gmail.com>
2023-02-01 18:47:16 +0000

    move files

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,39 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 22,
+  location: "New York"
+};
+
+const student2: Student = {
+  firstName: "Jane",
+  lastName: "Doe",
+  age: 21,
+  location: "Los Angeles"
+};
+
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement("table");
+
+studentsList.forEach(student => {
+  const row = document.createElement("tr");
+  const firstNameCell = document.createElement("td");
+  firstNameCell.textContent = student.firstName;
+  row.appendChild(firstNameCell);
+  
+  const locationCell = document.createElement("td");
+  locationCell.textContent = student.location;
+  row.appendChild(locationCell);
+  
+  table.appendChild(row);
+});
+
+document.body.appendChild(table);
